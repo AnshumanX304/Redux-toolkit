b55b2fde7c6c2236a341b2ef2cf6e5ed907974ad
Anshuman Tiwari <anshuman21153045@akgec.ac.in>
2024-07-30 22:13:17 +0530

    configured tailwind and used selectors to get data from store and display it

--- src/app/store.js
+++ src/app/store.js
@@ -2,7 +2,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "../features/todo/todoSlice.js"
 
 export const store = configureStore({
-    reducer: {
-        todo: todoReducer
-    }
+    reducer:todoReducer
 });
